aa50c6f14340b538da190ccd75dc37a82f84d4d5
Michael Kennedy <mikeckennedy@gmail.com>
2024-12-04 15:21:06 -0800

    Make runnable on Linux.

--- book/ch11-example-setup/scripts/build_containers.sh
+++ book/ch11-example-setup/scripts/build_containers.sh
@@ -1,26 +1,33 @@
+
+echo "Building containers"
+
 # ################ Set up environment for shared vars #####################
 set -eu pipefail # Enable strict error handling
 # Script's working directory
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-# echo "SCRIPT_DIR ${SCRIPT_DIR}"
+echo "SCRIPT_DIR ${SCRIPT_DIR}"
 RELATIVE_PATH="${SCRIPT_DIR}/../../.."
-# echo "RELATIVE_PATH ${RELATIVE_PATH}"
+echo "RELATIVE_PATH ${RELATIVE_PATH}"
 # Repo cloned here
 REPO_PATH=$(realpath "$RELATIVE_PATH")
-# echo "REPO_PATH ${REPO_PATH}"
+echo "REPO_PATH ${REPO_PATH}"
 # ############### END Set up environment ##################################
 
-
+echo "Building base images"
 cd ${REPO_PATH}/book/ch11-example-setup/containers/base-images
 docker compose build
 docker compose up -d
+echo "base images done"
 
 
+echo "Build core app"
 cd ${REPO_PATH}/book/ch11-example-setup/containers/core-app
 docker compose build
 docker compose up -d
+echo "core app done"
 
-
+echo "Building nginx server"
 cd ${REPO_PATH}/book/ch11-example-setup/containers/web-servers
 docker compose build
 docker compose up -d
+echo "nginx server done"
