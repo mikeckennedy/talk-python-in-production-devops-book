8ec80d169f9ada923be9f166b03d7bb3ad30e25e
Michael Kennedy <mikeckennedy@gmail.com>
2024-12-04 23:09:53 +0000

    tweak for paths and running on linux

--- book/ch11-example-setup/scripts/deploy.sh
+++ book/ch11-example-setup/scripts/deploy.sh
@@ -1,11 +1,14 @@
 # ################ Set up environment for shared vars #####################
-set -euo pipefail # Enable strict error handling
+set -e pipefail # Enable strict error handling
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)" # Script's working directory
-RELATIVE_PATH="${SCRIPT_DIR}/../../.."
+echo "SCRIPT_DIR ${SCRIPT_DIR}"
+RELATIVE_PATH="${SCRIPT_DIR}/../.."
+echo "RELATIVE_PATH ${RELATIVE_PATH}"
 REPO_PATH=$(realpath "$RELATIVE_PATH") # Repo cloned here
+echo "REPO_PATH ${REPO_PATH}"
 # ############### END Set up environment ##################################
 
 
-${REPO_PATH}/src/book/ch11-example-setup/scripts/update_images.sh
-${REPO_PATH}/book/ch11-example-setup/scripts/update_source.sh
+${REPO_PATH}/book/ch11-example-setup/scripts/update-images.sh
+${REPO_PATH}/book/ch11-example-setup/scripts/update-source.sh
 ${REPO_PATH}/book/ch11-example-setup/scripts/build_containers.sh
