fcab8c6fddbc3d41c931d2a4563a5293a93de142
Michael Kennedy <mikeckennedy@gmail.com>
2024-12-04 15:24:50 -0800

    Debugging

--- book/ch11-example-setup/scripts/build_containers.sh
+++ book/ch11-example-setup/scripts/build_containers.sh
@@ -4,14 +4,14 @@ echo "Building containers"
 # ################ Set up environment for shared vars #####################
 set -eu pipefail # Enable strict error handling
 # Script's working directory
-echo "BASE SOURCE: ${BASH_SOURCE[0]}"
+# echo "BASE SOURCE: ${BASH_SOURCE[0]}"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-echo "SCRIPT_DIR ${SCRIPT_DIR}"
+# echo "SCRIPT_DIR ${SCRIPT_DIR}"
 RELATIVE_PATH="${SCRIPT_DIR}/../../.."
-echo "RELATIVE_PATH ${RELATIVE_PATH}"
+# echo "RELATIVE_PATH ${RELATIVE_PATH}"
 # Repo cloned here
 REPO_PATH=$(realpath "$RELATIVE_PATH")
-echo "REPO_PATH ${REPO_PATH}"
+# echo "REPO_PATH ${REPO_PATH}"
 # ############### END Set up environment ##################################
 
 echo "Building base images"
