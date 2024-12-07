c1e0a9ce54d0027d760ed6f4dbc0e5f5c67f8367
Michael Kennedy <mikeckennedy@gmail.com>
2024-12-07 09:02:36 -0800

    Use the local copy for nginx configs in prod.

--- book/ch11-example-setup/containers/web-servers/dot-env-template.sh
+++ book/ch11-example-setup/containers/web-servers/dot-env-template.sh
@@ -14,7 +14,7 @@ CERTBOT_WWW=./cluster-data/nginx/certbot/www
 # Some candidate production settings
 NGINX_STATIC=/cluster-data/nginx/static
 NGINX_LOGS=/cluster-data/nginx/logs
-NGINX_SITES_ENABLED=/cluster-data/nginx/sites-enabled
+NGINX_SITES_ENABLED=./nginx-base-configs
 NGINX_LETS_ENCRYPT_ETC=/cluster-data/nginx/letsencrypt-etc
 NGINX_LETSENCRYPT_WWW=/cluster-data/nginx/letsencrypt-www
 CERTBOT_WWW=/cluster-data/nginx/certbot/www
